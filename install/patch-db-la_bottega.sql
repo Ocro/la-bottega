d6261c8334e0e72cf52647882affdb067a896799
David Miserez <david.miserez@altux.ch>
2019-03-19 08:08:00 +0100

    Rename to lowercase database tables in patch

--- install/patch-db-la_bottega.sql
+++ install/patch-db-la_bottega.sql
@@ -1,3 +1,7 @@
+-- rename tablename
+RENAME TABLE `la_bottega`.`Clients` TO `la_bottega`.`clients`;
+RENAME TABLE `la_bottega`.`Services` TO `la_bottega`.`services`;
+
 -- rename clients names
 ALTER TABLE `clients` CHANGE `ClientID` `id` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `Nom` `name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `Prénom` `firstname` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `N° Téléphone` `phone` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `Adresse` `address` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `Code Postale` `npa` INT(11) NULL DEFAULT '0', CHANGE `Ville` `city` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
@@ -5,11 +9,11 @@ ALTER TABLE `clients` CHANGE `ClientID` `id` INT(11) NOT NULL AUTO_INCREMENT, CH
 ALTER TABLE `services` CHANGE `ServiceID` `id` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `Service` `service` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `Prix` `price` INT(11) NULL DEFAULT '0';
 
 -- rename appointment names
-RENAME TABLE `la_bottega`.`visites` TO `la_bottega`.`appointment`;
+RENAME TABLE `la_bottega`.`Visites` TO `la_bottega`.`appointment`;
 ALTER TABLE `appointment` CHANGE `VisiteID` `idappointment` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `ClientID` `idclient` INT(11) NULL DEFAULT '0', CHANGE `Date` `date` DATETIME NULL DEFAULT NULL;
 
 -- rename appointmentdetails names
-RENAME TABLE `la_bottega`.`visitesdetail` TO `la_bottega`.`appointmentdetails`;
+RENAME TABLE `la_bottega`.`VisitesDetail` TO `la_bottega`.`appointmentdetails`;
 ALTER TABLE `appointmentdetails` CHANGE `VisiteID` `idappointment` INT(11) NOT NULL DEFAULT '0', CHANGE `ServiceID` `idservice` INT(11) NOT NULL DEFAULT '0', CHANGE `Prix` `price` INT(11) NULL DEFAULT '0', CHANGE `Couleur` `color` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `Remarque` `comment` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
 -- Add contraints
